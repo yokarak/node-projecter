e26b664f98fbf2fdc866ce33de1865d1b5f74cef
yokarak <54890038+yokarak@users.noreply.github.com>
2020-09-09 10:03:07 +0200

    moved to folder

--- HelloWorld.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello wolrd!")
-console.log(2+2)
-
-var ts = new Date();
-console.log(ts.toDateString())
---
+++ learning/HelloWorld.js
@@ -0,0 +1,5 @@
+console.log("Hello wolrd!")
+console.log(2+2)
+
+var ts = new Date();
+console.log(ts.toDateString())
